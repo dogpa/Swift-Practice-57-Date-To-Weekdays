a7280081144d7be30520dd21e0d7ca6a739465c2
DogpaChen <dogpa0130@gmail.com>
2021-08-10 14:55:42 +0800

    Initial Commit

---
+++ Swift Practice # 57 Date To Weekdays/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 57 Date To Weekdays
+//
+//  Created by CHEN PEIHAO on 2021/8/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
